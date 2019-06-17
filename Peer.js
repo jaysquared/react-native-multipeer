909bf48e110ba76273fc36c923fe4bdcc298191a
Jeremy Cheng <jeremy@atvenu.com>
2019-06-17 11:55:05 -0600

    Added additional Parameter used for adding additional data to the advertise discovery info

--- Peer.js
+++ Peer.js
@@ -1,9 +1,10 @@
 import { EventEmitter } from 'events';
 
 export default class Peer extends EventEmitter {
-  constructor(id, name) {
+  constructor(id, name, other) {
     super();
     this.id = id;
     this.name = name;
+    this.other = other
   }
 }
